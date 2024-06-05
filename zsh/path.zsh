cf796f8c9422ef916d01eba620cad98045f0f1aa
Laurens Groeneveld <me@laurens.io>
2024-06-05 10:43:51 +0200

    Project settings

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -12,3 +12,12 @@ export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 # Load unversioned Python binaries
 export PATH="/usr/local/opt/python@3.11/libexec/bin:$PATH"
+
+# Java (GVB)
+export JAVA_HOME=/usr/local/Cellar/openjdk@17/17.0.11
+export PATH=${PATH}:${JAVA_HOME}/bin
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
